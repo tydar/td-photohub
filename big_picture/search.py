ca917394ffb51679863e1ecf0309558c0a37a8de
Tyler Darnell <tyler.a.darnell@gmail.com>
2021-05-06 07:19:57 -0500

    add mockouts for search views

---
+++ big_picture/search.py
@@ -0,0 +1,11 @@
+from flask import render_template, Blueprint
+
+bp = Blueprint('search', __name__, url_prefix='/search')
+
+@bp.route('/')
+def gallery():
+    return render_template('search/simple.html')
+
+@bp.route('/advanced')
+def add_image():
+    return render_template('search/advanced.html')
