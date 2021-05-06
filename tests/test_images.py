eb5a27b41b6e16bf9770d7c33bf570ccff2ca06e
Tyler Darnell <tyler.a.darnell@gmail.com>
2021-05-06 07:15:28 -0500

    passing tests after adding image blueprint to app factory

--- tests/test_images.py
+++ tests/test_images.py
@@ -4,7 +4,7 @@ def test_hello(client, app):
     assert client.get('/hello').status_code == 200
 
 def test_gallery(client, app):
-    assert client.get('/images').status_code == 200
+    assert client.get('/images/').status_code == 200
 
 def test_add(client, app):
     assert client.get('/images/add').status_code == 200
