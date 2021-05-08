d7c5ab0ac33eda5ab9e3af3418a3623fae225b93
Tyler Darnell <tyler.a.darnell@gmail.com>
2021-05-07 20:41:58 -0500

    filter by both desc and title & send to list view

--- big_picture/search.py
+++ big_picture/search.py
@@ -4,6 +4,13 @@ bp = Blueprint('search', __name__, url_prefix='/search')
 
 @bp.route('/', methods=['GET', 'POST'])
 def gallery():
+    if request.method == 'POST':
+        search = request.form['search']
+        res_list = Image.query.filter(
+            Image.title.match(search),
+            Image.description.match(search)
+        ).all()
+        return render_template('search/results.html', res_list=res_list)
     return render_template('search/simple.html')
 
 @bp.route('/advanced')
